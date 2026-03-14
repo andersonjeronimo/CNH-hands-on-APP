434ddb44aef47d3dc4b534b46339848190367b39
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2026-03-14 11:27:33 -0300

    password form field

--- frontend/src/pages/RegisterForm.tsx
+++ frontend/src/pages/RegisterForm.tsx
@@ -39,11 +39,21 @@ function RegisterForm() {
 
     useEffect(() => {
         setProvinceData(Estados);
+        setFormData(customerModel);
     }, []);
 
     const [formData, setFormData] = useState(customerModel);
     const [isCpf, setIsCpf] = useState(true);
     const [isCnpj, setIsCnpj] = useState(false);
+    const [passwordTest, setPasswordTest] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [showPasswordTest, setShowPasswordTest] = useState(false);
+    const [passwordFieldMessage, setPasswordFieldMessage] = useState("");
+
+    const handlePwdInputChange = (e: any) => {
+        const { value } = e.target;
+        setPasswordTest(value);
+    }
 
     const handleCpfCnpjRadioChange = (e: any) => {
         const { name, checked } = e.target;
@@ -55,6 +65,17 @@ function RegisterForm() {
         } else {
             setIsCpf(false);
             setIsCnpj(true);
+        }
+    }
+
+    const handleShowPassword = async (e: any) => {
+        e.preventDefault();
+        const { id } = e.target;
+        if (id === 'form_password') {
+            setShowPassword(!showPassword);
+        }
+        if (id === 'form_password_test') {
+            setShowPasswordTest(!showPasswordTest);
         }
     }
 
@@ -187,6 +208,16 @@ function RegisterForm() {
                 ...prevState,
                 ['cnpj']: ''
             }));
+        } else if (formData.password !== passwordTest) {
+            setInputClass(inputFocusClass.danger);
+            setAlertClass(messageClass.danger);
+            setMessage(`Atenção: As senhas informadas não são iguais`);
+            setFormData(prevState => ({
+                ...prevState,
+                ['password']: ''
+            }));
+            setPasswordTest('');
+            setPasswordFieldMessage(`As senhas informadas não são iguais`)
         } else {
             //Prosseguir Cadastro de instrutores. Preencha os campos obrigatórios
             setInputClass(inputFocusClass.default);
@@ -210,7 +241,7 @@ function RegisterForm() {
                                 }));
                             }
                         } else {
-                            //setMessage('Response data is empty or null.');
+                            //setMessage('Response data is empty or null.');                            
                             axios.post(import.meta.env.VITE_INSTRUCTOR_API_URL, formData)
                                 .then((response) => {
                                     navigate('/register-result', { state: response.data });
@@ -342,16 +373,76 @@ function RegisterForm() {
                     <div className='col-md-6'>
                         <label className='form-label'>6 - Email</label>
                         <div className='input-group'>
-                            <span className='input-group-text' id='email'>@</span>
+                            <span className='input-group-text' id='email'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-at" viewBox="0 0 16 16">
+                                    <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z" />
+                                    <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z" />
+                                </svg>
+
+                            </span>
                             <input type='email' className='form-control' name='email' id='email'
                                 value={formData.email} onChange={handleInputChange}
                                 aria-describedby='email' required />
                         </div>
                     </div>
 
+                    <div className='col-md-3'>
+                        <label className='form-label'>7 - Senha</label>
+                        <div className='input-group'>
+                            <span className='input-group-text' id='form_password' onClick={handleShowPassword}>
+                                {
+                                    showPassword ?
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                                        </svg>
+                                        :
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash" viewBox="0 0 16 16">
+                                            <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
+                                            <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
+                                            <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                                        </svg>
+                                }
+                            </span>
+                            <input type={showPassword ? 'text' : 'password'} className='form-control' name='password' id='password'
+                                value={formData.password} onChange={handleInputChange}
+                                aria-describedby='passwordHelpBlock' required />
+                        </div>
+                        <div id="passwordHelpBlock" className="form-text">
+                            <strong>{passwordFieldMessage}</strong>
+                        </div>
+                    </div>
+
+                    <div className='col-md-3'>
+                        <label className='form-label'>8 - Repetir Senha</label>
+                        <div className='input-group'>
+                            <span className='input-group-text' id='form_password_test' onClick={handleShowPassword}>
+                                {
+                                    showPasswordTest ?
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                                        </svg>
+                                        :
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash" viewBox="0 0 16 16">
+                                            <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
+                                            <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
+                                            <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                                        </svg>
+                                }
+                            </span>
+                            <input type={showPasswordTest ? 'text' : 'password'} className='form-control' name='password_test' id='password_test'
+                                value={passwordTest} onChange={handlePwdInputChange}
+                                aria-describedby='passwordHelpBlock' required />
+                        </div>
+                        <div id="passwordHelpBlock" className="form-text">
+                            <strong>{passwordFieldMessage}</strong>
+                        </div>
+                    </div>
+
 
                     <div className='col-md-1'>
-                        <label className='form-label'>7 - DDD</label>
+                        <label className='form-label'>9 - DDD</label>
                         <select name='ddd' id='ddd' className='form-select' value={formData.ddd} onChange={handleInputChange} required>
                             <option selected value={'00'}>00</option>
                             {selectedProvince.ddd.map((ddd) => (
@@ -362,15 +453,23 @@ function RegisterForm() {
                         </select>
                     </div>
 
-                    <div className='col-md-5'>
-                        <label className='form-label'>8 - Celular | Whatsapp</label>
-                        <input type='number' className='form-control' name='phone' id='phone' min='10000000' max='999999999'
-                            value={formData.phone} onChange={handleInputChange}
-                            placeholder='Apenas números, sem DDD, pontos ou traços. Ex.: 9 9888 9999' required />
+                    <div className='col-md-6'>
+                        <label className='form-label'>10 - Celular | Whatsapp</label>
+                        <div className='input-group'>
+                            <span className='input-group-text' id='email'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
+                                    <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                                </svg>
+                            </span>
+                            <input type='number' className='form-control' name='phone' id='phone' min='10000000' max='999999999'
+                                value={formData.phone} onChange={handleInputChange}
+                                placeholder='Apenas números, sem DDD, pontos ou traços. Ex.: 9 9888 9999' required />
+                        </div>
+
                     </div>
 
-                    <div className='col-md-6'>
-                        <label className='form-label'>9 - CPF</label>
+                    <div className='col-md-5'>
+                        <label className='form-label'>11 - CPF</label>
                         <div className="form-check">
                             <input className="form-check-input"
                                 type="radio"
@@ -388,7 +487,7 @@ function RegisterForm() {
                     </div>
 
                     <div className='col-md-6'>
-                        <label className='form-label'>9 - CNPJ</label>
+                        <label className='form-label'>12 - CNPJ</label>
                         <div className="form-check">
                             <input className="form-check-input"
                                 type="radio"
@@ -410,7 +509,7 @@ function RegisterForm() {
                     </div>
 
                     <div className='col-md-12'>
-                        <label className='form-label'>10 -Descrição do veículo (opcional)</label>
+                        <label className='form-label'>13 -Descrição do veículo (opcional)</label>
                         <textarea className='form-control' name='description' id='description'
                             value={formData.description} onChange={handleInputChange} rows={3}
                             placeholder='Se for veículo próprio, você pode colocar aqui a marca, modelo, tipo de câmbio, etc. Isso pode ajudar na escolha do instrutor'></textarea>
@@ -419,7 +518,7 @@ function RegisterForm() {
                     <hr />
 
                     <div className='col-md-4'>
-                        <label className='form-label'>11 - Status</label>
+                        <label className='form-label'>14 - Status</label>
                         <div className='form-check'>
                             <input className='form-check-input'
                                 type='radio'
@@ -459,7 +558,7 @@ function RegisterForm() {
                     </div>
 
                     <div className='col-md-4'>
-                        <label className='form-label'>12 - Categoria</label>
+                        <label className='form-label'>15 - Categoria</label>
                         <div className='form-check'>
                             <input className='form-check-input'
                                 type='radio'
@@ -499,7 +598,7 @@ function RegisterForm() {
                     </div>
 
                     <div className='col-md-4'>
-                        <label className='form-label'>13 - Veículo</label>
+                        <label className='form-label'>16 - Veículo</label>
                         <div className='form-check'>
                             <input className='form-check-input'
                                 type='radio'
@@ -541,7 +640,7 @@ function RegisterForm() {
                     <hr />
 
                     <div className='col-md-12'>
-                        <label className='form-label'>14 - Termos e Condições Gerais de uso</label>
+                        <label className='form-label'>17 - Termos e Condições Gerais de uso</label>
                         <div className="form-check">
                             <input className="form-check-input"
                                 type="checkbox"
