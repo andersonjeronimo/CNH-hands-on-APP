3706981396ad3af5954b1a0c1745caab161ec507
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2026-04-20 17:47:49 -0300

    Details

--- frontend/src/pages/SearchResult.tsx
+++ frontend/src/pages/SearchResult.tsx
@@ -148,13 +148,12 @@ function SerchResult() {
                 </a>
             </div>
             <hr />
-            <table className="table">
+            <table className="table text-center">
                 <thead>
                     <tr className='table-light'>
                         <th scope="col">Foto</th>
-                        <th scope="col">Instrutor</th>
-                        <th scope="col">Categoria</th>
-                        <th scope='col'>Conversar</th>
+                        <th scope="col">Instrutor</th>                        
+                        <th scope='col'>Detalhes</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -175,15 +174,12 @@ function SerchResult() {
                                 </td>
                                 <td>
                                     <p className="fs-4">{data.firstname}</p>
-                                </td>
-                                <td>
-                                    <p className="fs-4">{data.category}</p>
-                                </td>
+                                </td>                                
                                 <td>
                                     <a href="#" role="button" data-bs-toggle="modal" data-bs-target={`#${data.userId}`}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
-                                            <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
-                                            <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9 9 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.4 10.4 0 0 1-.524 2.318l-.003.011a11 11 0 0 1-.244.637c-.079.186.074.394.273.362a22 22 0 0 0 .693-.125m.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6-3.004 6-7 6a8 8 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a11 11 0 0 0 .398-2" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" className="bi bi-person-vcard" viewBox="0 0 16 16">
+                                            <path d="M5 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4m4-2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5M9 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 9 8m1 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5" />
+                                            <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H8.96q.04-.245.04-.5C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 1 1 12z" />
                                         </svg>
                                     </a>
                                     <ChatModal id={data.userId} data={data}></ChatModal>
